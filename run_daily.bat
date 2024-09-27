4e279bb0df82ab2bcebd21025cdf713f227fffec
Ian <ianhyheung@gmail.com>
2024-09-27 14:21:53 +0100

    Added ticker saving to batch file

--- run_daily.bat
+++ run_daily.bat
@@ -5,7 +5,9 @@ ECHO Saving insider transaction data from yesterday
 cd scripts
 
 CALL conda activate your_env_name
+
 python live_save_insider.py
+python ticker_prices.py
 
 ECHO Data saved successfully
 PAUSE
