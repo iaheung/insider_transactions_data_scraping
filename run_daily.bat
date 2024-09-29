846382f65fa60d2be11f63fe0b95bbdb130554f9
Ian Heung （香愷仁） <98202025+iaheung@users.noreply.github.com>
2024-09-29 17:03:37 +0100

    Update run_daily.bat

--- run_daily.bat
+++ run_daily.bat
@@ -3,6 +3,10 @@
 ECHO Saving insider transaction data from yesterday
 
 cd scripts
+IF ERRORLEVEL 1 (
+    ECHO Failed to navigate to the scripts directory
+    EXIT /B 1
+)
 
 CALL conda activate your_env_name
 
