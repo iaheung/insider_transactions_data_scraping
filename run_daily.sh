1e422a7878474f98dcb20be29576eee292f9106a
Ian Heung （香愷仁） <98202025+iaheung@users.noreply.github.com>
2024-09-29 17:04:56 +0100

    Create run_daily.sh

---
+++ run_daily.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Saving insider transaction data from yesterday"
+
+cd scripts || { echo "Failed to navigate to the scripts directory"; exit 1; }
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate your_env_name
+
+python live_save_insider.py
+python ticker_prices.py
+
+echo "Data saved successfully"
